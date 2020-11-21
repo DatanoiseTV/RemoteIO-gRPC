31c7e2d004442d2bab7901ef1a2790083859e587
DatanoiseTV <de.retrospektive@gmail.com>
2020-11-21 12:43:51 +0100

    Add LED I/O example.

--- remoteio_client/main.go
+++ remoteio_client/main.go
@@ -2,15 +2,18 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
+	"os"
+	"os/signal"
 	remoteio "remoteio/rio"
 	"time"
 )
 
 const (
-	address     = "localhost:9000"
+	address     = "patchbox.local:9000"
 	defaultName = "world"
 )
 
@@ -26,10 +29,40 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	r, err := c.PinMode(ctx, &remoteio.PinModeMessage{Pin: 1, Mode: remoteio.PinModeMessage_ANALOG_IN})
+	r, err := c.PinMode(ctx, &remoteio.PinModeMessage{Pin: 17, Mode: remoteio.PinModeMessage_DIGITAL_OUT})
 	if err != nil {
 		log.Fatal("Did not get message.")
 	}
 	log.Printf("Response: %v", r.GetPin())
 
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	go func() {
+		<-sigs
+		fmt.Printf("You pressed ctrl + C. User interrupted infinite loop.")
+		os.Exit(0)
+	}()
+	BlinkLED(c)
+
+
+
+}
+
+func BlinkLED(c remoteio.RemoteIOClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	state := false
+	for {
+		r, err := c.DigitalWrite(ctx, &remoteio.DigitalState{Pin: 17, State: state})
+		if err != nil {
+			log.Println("Did not get response.")
+		} else {
+			log.Printf("Response: %v", r.GetPin())
+		}
+
+		state = !state
+		time.Sleep(time.Millisecond * 50)
+	}
 }
